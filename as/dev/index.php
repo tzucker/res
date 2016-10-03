bf0e6729145abaca74e751a823918deb61767ee1
Thomas Zucker-Scharff <thomas.zucker-scharff@einstein.yu.edu>
2016-10-02 23:20:29 -0400

    Add Resume Project

---
+++ as/dev/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width" />
+
+	<title>Thomas Zucker-Scharff's Resume</title>
+
+	<!-- Included CSS Files -->
+	<link rel="stylesheet" href="../../stylesheets/foundation.css">
+	<link rel="stylesheet" href="../../stylesheets/app.css">
+	<link rel="stylesheet" href="../../stylesheets/user.css">
+	<link href='http://fonts.googleapis.com/css?family=Playfair+Display|EB+Garamond' rel='stylesheet' type='text/css'>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="stylesheets/ie.css">
+	<![endif]-->
+
+	<script src="../../javascripts/modernizr.foundation.js"></script>
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link rel="stylesheet" href="../../stylesheets/print.css" media="print">
+
+</head>
+<body>
+
+	<!-- container -->
+	<div class="container">
+		<header>
+			<div class="row">
+				<div id="topid" class="twelve columns">
+					<h2>Thomas Zucker-Scharff</h2>
+					<h5>4 Harrison Drive &#149; Larchmont, NY, 10538 &#149; (914) 274-1048</h5>
+					<hr />
+				</div>
+			</div>
+		</header>
+		<div class="row">
+			<div class="row">
+				<div class="two columns">
+					<h4 class="section-title">Skills:</h4>
+				</div>
+				<div class="ten columns">
+					<p><b>PHP</b>, <b>MySQL</b>, <b>HTML5</b>, <b>CSS3</b>, <b>jQuery</b>, <b>SASS</b>, <b>Adobe Creative Suite</b>, <b>WordPress</b>, WordPressMS, MailChimp, Google AdWords, <b>Google Analytics</b>, OpenX, <b>SEO</b>, <b>Social Media Optimization</b>, Responsive Design, Convergent media, <b>AP Style</b>, Drupal, Blogger, QuarkXPress, Audacity, CoverItLive, Livestream.com, TextPattern, Prezi, Omeka, <b>Git</b>, <b>Usability</b>, UX Testing</p>
+				</div>
+			</div>
+
+			<hr />
+		</div>
+		<div class="row">
+			<div class="row title-area">
+				<div class="twelve columns">
+					<h4 class="section-title">Selected Experience</h4>
+				</div>
+			</div>
+			<div class="row item">
+				<div class="two columns">
+					1992-Present
+				</div>
+				<div class="ten columns">
+					<div class="left joblocation"><p><span class="item-title">Albert Einstein College of Medicine</span></p></div><div class="right location">Bronx, NY</div>
+					<div class="clearfloat">
+						<p><span class="jobtitle">Systems Analyst</span></p>
+						<ul class="jobaccomp">
+							<li>Social Media Advocate for institutional uses</li>
+							<li>Database Development and design - Design, planning, development and creation of various databases to assist in running of the Cancer Center</li>
+							<li>Web Development</li>
+							<li>IT SecurityImplementation of colleeg security protocols for computing and Social Media</li>
+							<li>Software and Hardware recommendation and evaluation</li>
+							<li>Troubleshoot AECC Network File Share</li>
+							<li>Maintain/Troubleshoot/Train end users on AECC computers and A/V equipment</li>
+							<li>Shared equipment support</li>
+							<li>Maintain AD OU for Cancer Center</li>
+							<li>Track Grant and Publication Compliance</li>
+							<li>Trainer/Instructor for NIH related information systems</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row item">
+				<div class="two columns">
+					2009-Present
+				</div>
+				<div class="ten columns">
+					<div class="left joblocation"><p><span class="item-title">Experts Echange</span></p></div><div class="right location">San Luis Obispo, CA</div>
+					<div class="clearfloat">
+						<p><span class="jobtitle">Member / Expert</span></p>
+						<ul class="jobaccomp">
+							<li>Participating Expert at experts-exchange.com</li>
+                            				<li>http://www.experts-exchange.com/members/tzucker.html</li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
+			<div class="row item">
+				<div class="two columns">
+					March 2015-April 2015
+				</div>
+				<div class="ten columns">
+					<div class="left joblocation"><p><span class="item-title">Adjunct Instructor</span></p></div><div class="right location">New York, NY</div></div>
+					<div class="clearfloat">
+						<p><span class="jobtitle">Adjunct  insstucto, Lehman College</span></p>
+						<ul class="jobaccomp">
+							<li>Instructor for Mathematics common core course for 7th and 8th grades</li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
+		<div class="row">
+
+				<div class="two columns">
+					<h4 class="section-title">Awards:</h4>
+				</div>
+				<div class="ten columns">
+					<p>
+						<span class="item-title">Certified Anti-Virus Applications Wizard</span> - Experts Exchange <br />
+						<span class="item-title">Certified Anti-Spyware Guru</span> - Experts Exchange <br />
+						<span class="item-title">Certified Windows 7 Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Hard Drives and Storage Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified IT Administration Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Personal Computers Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Windows XP Operating System Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Networking Security Vulnerabilities Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Web Browsers Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Backup & Resore Software Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Security Master</span> - Experts Exchange <br />
+						<span class="item-title">Certified Windows 8 Master</span> - Experts Exchange <br />
+					</p>
+				</div>
+			<div class="row"></div>
+
+			<hr />
+		</div>
+		<div class="row">
+			<div class="row">
+				<div class="two columns">
+					<h4 class="section-title">Education</h4>
+				</div>
+			</div>
+			<div class="row degree">
+				<div class="two columns">
+
+				</div>
+				<div class="eight columns">
+					<h3 class="item-title">Northeastern University</h3>
+					<p><span class="jobtitle">Bachelor of Science in Elementary Education</span></p>
+				</div>
+				<div class="two columns edlocal">
+					<p>Boston MA</p>
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+	<!-- container -->
+
+
+
+
+	<!-- Included JS Files -->
+	<script src="../../javascripts/jquery.min.js"></script>
+	<script src="../../javascripts/foundation.js"></script>
+	<script src="../../javascripts/app.js"></script>
+
+</body>
+</html>
